775915fe923491fcb3103c03296342caaa3ad3e8
jshong0907 <jshong0907@gmail.com>
2024-09-27 16:57:55 +0900

    Spring inmemory config

---
+++ src/test/java/org/example/j3b3/J3b3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.j3b3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class J3b3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
