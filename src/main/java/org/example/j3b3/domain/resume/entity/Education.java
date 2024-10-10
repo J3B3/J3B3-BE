349d239983fa4e160116a89b8929b4f59b418610
jshong0907 <jshong0907@gmail.com>
2024-10-11 03:58:02 +0900

    :card_file_box: nullable 관련 제약 추가 및 validation 사용

--- src/main/java/org/example/j3b3/domain/resume/entity/Education.java
+++ src/main/java/org/example/j3b3/domain/resume/entity/Education.java
@@ -1,6 +1,9 @@
 package org.example.j3b3.domain.resume.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.example.j3b3.global.util.BaseEntity;
@@ -16,27 +19,34 @@ public class Education extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(length = 20)
+    @Size(max = 20)
+    @NotNull
     @Enumerated(EnumType.STRING)
     private EducationType educationType;
 
-    @Column(length = 50)
+    @Size(max = 50)
+    @NotBlank
     private String schoolName;
 
+    @NotNull
     private LocalDate admissionDate;
 
     private LocalDate graduationDate;
 
-    @Column(length = 20)
+    @Size(max = 20)
+    @NotNull
     @Enumerated(EnumType.STRING)
     private GraduationType graduationType;
 
-    @Column(length = 50)
+    @Size(max = 50)
+    @NotBlank
     private String major;
 
+    @NotNull
     @Column(precision = 3, scale = 2)
     private BigDecimal major_gpa;
 
+    @NotNull
     @Column(precision = 2, scale = 1)
     private BigDecimal max_gpa;
 }
