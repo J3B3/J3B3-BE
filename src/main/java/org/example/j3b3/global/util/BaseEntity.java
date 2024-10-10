7ed197517f776c7b4d545a435dcebd21f68b1d25
jshong0907 <jshong0907@gmail.com>
2024-10-11 03:58:23 +0900

    :card_file_box: 생성일 및 수정일 notnull 설정

--- src/main/java/org/example/j3b3/global/util/BaseEntity.java
+++ src/main/java/org/example/j3b3/global/util/BaseEntity.java
@@ -2,6 +2,7 @@ package org.example.j3b3.global.util;
 
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,9 +12,11 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public abstract class BaseEntity {
+    @NotNull
     @CreatedDate
     private LocalDateTime createdAt;
 
+    @NotNull
     @LastModifiedDate
     private LocalDateTime updatedAt;
 }
